7088327ca93bd75c71075e908f0b5b979cbf2adf
Martin Sosic <sosic.martin@gmail.com>
2023-11-29 23:26:37 +0100

    Made day01 read data for typescript.

--- 2023/typescript/src/day01.ts
+++ 2023/typescript/src/day01.ts
@@ -1,1 +1,8 @@
-console.log("hi");
+import * as fs from "fs";
+
+async function day01(): Promise<void> {
+  const inputText: string = fs.readFileSync("data/day01-input.txt", { encoding: "utf8" });
+  console.log(inputText);
+}
+
+day01();
