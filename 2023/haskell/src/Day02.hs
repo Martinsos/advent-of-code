ad2445d9be4f38916bf8d9b6d1c7ecb3e9513959
Martin Sosic <sosic.martin@gmail.com>
2023-12-02 13:01:57 +0100

    Fixed TS day01-2, implemented hs day02-1.

--- 2023/haskell/src/Day02.hs
+++ 2023/haskell/src/Day02.hs
@@ -1,10 +1,46 @@
 module Day02 where
 
+import Data.List.Split (splitOn)
 import Paths_aoc2023 (getDataFileName)
+
+-- Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+-- Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+-- Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+-- Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+-- Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
 
 day02 :: IO ()
 day02 = do
   inputLines <- lines <$> (getDataFileName "day02-input.txt" >>= readFile)
-  putStrLn "This is what I read from input:"
-  putStrLn $ unlines inputLines
-  putStrLn "TODO: implement Day 02"
+  print $ sum $ map fst $ filter (isGamePossible . snd) $ zip [1 :: Int ..] $ parseGame <$> inputLines
+
+data CubeSet = CubeSet {red :: Int, green :: Int, blue :: Int}
+  deriving (Show)
+
+instance Monoid CubeSet where
+  mempty = CubeSet 0 0 0
+
+instance Semigroup CubeSet where
+  (CubeSet a b c) <> (CubeSet a' b' c') = CubeSet (a + a') (b + b') (c + c')
+
+parseGame :: String -> [CubeSet]
+parseGame = (parseCubeSet <$>) . splitOn "; " . (!! 1) . splitOn ": "
+  where
+    parseCubeSet :: String -> CubeSet
+    parseCubeSet = mconcat . (parseCubeColorPick <$>) . splitOn ", "
+
+    parseCubeColorPick :: String -> CubeSet
+    parseCubeColorPick str = case splitOn " " str of
+      [num, color]
+        | color == "red" -> mempty {red = read num}
+        | color == "green" -> mempty {green = read num}
+        | color == "blue" -> mempty {blue = read num}
+      _ -> error "invalid cube color pick"
+
+isSubsetOf :: CubeSet -> CubeSet -> Bool
+isSubsetOf (CubeSet a b c) (CubeSet a' b' c') = a <= a' && b <= b' && c <= c'
+
+isGamePossible :: [CubeSet] -> Bool
+isGamePossible = all (`isSubsetOf` maxSet)
+  where
+    maxSet = CubeSet {red = 12, green = 13, blue = 14}
--- 2023/typescript/src/day01.ts
+++ 2023/typescript/src/day01.ts
@@ -4,10 +4,7 @@ import _ from "lodash";
 day01();
 
 function day01(): void {
-  const lines: string[] = fs
-    .readFileSync("data/day01-input.txt", { encoding: "utf8" })
-    .trim()
-    .split("\n");
+  const lines: string[] = fs.readFileSync("data/day01-input.txt", { encoding: "utf8" }).trim().split("\n");
   console.log("Part one:", _.sum(lines.map(findCalibrationNumber)));
   console.log("Part two:", _.sum(lines.map(spellingsToDigits).map(findCalibrationNumber)));
 }
@@ -17,17 +14,13 @@ function findCalibrationNumber(line: string): number {
   return parseInt(digits[0] + _.last(digits));
 }
 
-// TODO: There is a mistake here. We can't just transform all spellings to digits
-// from left to right becaue if line ends with oneight then we thing it is 1ight
-// but it should be on8, because when looking at last digit we need to do it from right.
-// So we should look for first spelling from left and for last spelling from right.
 function spellingsToDigits(line: string): string {
   if (line.length === 0) return line;
   const spellings = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
   for (let i = 0; i < spellings.length; i++) {
     const spelling = spellings[i];
     if (line.startsWith(spelling)) {
-      return (i + 1).toString() + spellingsToDigits(line.slice(spelling.length));
+      return (i + 1).toString() + spellingsToDigits(line.slice(1));
     }
   }
   return line[0] + spellingsToDigits(line.slice(1));
