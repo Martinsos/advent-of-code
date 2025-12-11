61fb30b9ad58e6a7bd2d1c51f8ad4b5f4efe3ab4
Martin Sosic <sosic.martin@gmail.com>
2025-12-11 23:02:47 +0100

    Solved day 06 part 01.

--- 2025/haskell/src/Day06.hs
+++ 2025/haskell/src/Day06.hs
@@ -1,35 +1,74 @@
 module Day06 where
 
-import Data.Either (fromRight)
+import Data.List (intercalate)
 import Data.Void
 import Paths_aoc (getDataFileName)
 import Test.Hspec (describe, hspec, shouldBe, specify)
 import qualified Text.Megaparsec as P
 import qualified Text.Megaparsec.Char as P
+import qualified Text.Megaparsec.Char.Lexer as L
 
 day06 :: IO (Integer, Integer)
 day06 = solve <$> (getDataFileName "day06-input.txt" >>= readFile)
 
 solve :: String -> (Integer, Integer)
-solve input = const (42, 314) $ parseInput input
+solve input = (uncurry sumOfAllProblems $ numbersAndOperators, undefined)
+  where
+    numbersAndOperators = parseInput input
+
+    sumOfAllProblems :: [[Integer]] -> [Operator] -> Integer
+    sumOfAllProblems numbersRows operatorsRow
+      | all null numbersRows && null operatorsRow = 0
+      | otherwise =
+          calc (head operatorsRow) (head <$> numbersRows)
+            + sumOfAllProblems (tail <$> numbersRows) (tail operatorsRow)
 
 type Parser = P.Parsec Void String
 
-parseInput :: String -> ()
-parseInput = fromRight (error "parse error") . P.parse inputP ""
+parseInput :: String -> ([[Integer]], [Operator])
+parseInput = either (error . P.errorBundlePretty) id . P.parse inputP ""
   where
-    inputP :: Parser ()
     inputP = do
-      _ <- P.string "test"
-      pure ()
+      numbersRows <- P.some $ P.try (numbersRowP <* P.eol)
+      operatorsRow <- operatorsRowP
+      return (numbersRows, operatorsRow)
+
+    numbersRowP :: Parser [Integer]
+    numbersRowP = P.hspace *> P.some (L.decimal <* P.hspace)
+
+    operatorsRowP :: Parser [Operator]
+    operatorsRowP = P.hspace *> P.some (operatorP <* P.hspace)
+
+    operatorP :: Parser Operator
+    operatorP = (Product <$ P.char '*') P.<|> (Sum <$ P.char '+')
+
+data Operator = Sum | Product deriving (Show, Eq)
+
+calc :: Operator -> [Integer] -> Integer
+calc Sum = sum
+calc Product = product
 
 ------------------------------------------------------------------------------------------
 
 tests :: IO ()
 tests = hspec $ do
-  describe "solve" $ do
-    specify "dummy test case" $
-      solve "test" `shouldBe` (42, 314)
+  let exampleInputText =
+        intercalate
+          "\n"
+          [ "123 328  51 64 ",
+            " 45 64  387 23 ",
+            "  6 98  215 314",
+            "*   +   *   +  "
+          ]
 
---  specify "day06" $ do
---    day03 >>= (`shouldBe` "actual solution")
+  describe "parseInput" $ do
+    specify "example input" $
+      parseInput exampleInputText
+        `shouldBe` ([[123, 328, 51, 64], [45, 64, 387, 23], [6, 98, 215, 314]], [Product, Sum, Product, Sum])
+
+  describe "solve" $ do
+    specify "example input" $
+      fst (solve exampleInputText) `shouldBe` 4277556
+
+  specify "day06" $ do
+    day06 >>= (`shouldBe` 5524274308182) . fst
--- 2025/haskell/src/Day07.hs
+++ 2025/haskell/src/Day07.hs
@@ -1,10 +1,35 @@
 module Day07 where
 
+import Data.Either (fromRight)
+import Data.Void
 import Paths_aoc (getDataFileName)
+import Test.Hspec (describe, hspec, shouldBe, specify)
+import qualified Text.Megaparsec as P
+import qualified Text.Megaparsec.Char as P
 
-day07 :: IO ()
-day07 = do
-  inputLines <- lines <$> (getDataFileName "day07-input.txt" >>= readFile)
-  putStrLn "This is what I read from input:"
-  putStrLn $ unlines inputLines
-  putStrLn "TODO: implement Day 07"
+day07 :: IO (Int, Int)
+day07 = solve <$> (getDataFileName "day07-input.txt" >>= readFile)
+
+solve :: String -> (Int, Int)
+solve input = const (42, 314) $ parseInput input
+
+type Parser = P.Parsec Void String
+
+parseInput :: String -> ()
+parseInput = fromRight (error "parse error") . P.parse inputP ""
+  where
+    inputP :: Parser ()
+    inputP = do
+      _ <- P.string "test"
+      pure ()
+
+------------------------------------------------------------------------------------------
+
+tests :: IO ()
+tests = hspec $ do
+  describe "solve" $ do
+    specify "dummy test case" $
+      solve "test" `shouldBe` (42, 314)
+
+--  specify "day07" $ do
+--    day07 >>= (`shouldBe` "actual solution")
