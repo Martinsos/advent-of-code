2299c5340ade4168daaa6f899f9a3d4d6247362e
Martin Sosic <sosic.martin@gmail.com>
2025-12-14 23:05:12 +0100

    Solved day 6 part 2.

--- 2025/haskell/src/Day06.hs
+++ 2025/haskell/src/Day06.hs
@@ -1,5 +1,6 @@
 module Day06 where
 
+import Data.Char (isSpace)
 import Data.List (intercalate)
 import Data.Void
 import Paths_aoc (getDataFileName)
@@ -9,12 +10,14 @@ import qualified Text.Megaparsec.Char as P
 import qualified Text.Megaparsec.Char.Lexer as L
 
 day06 :: IO (Integer, Integer)
-day06 = solve <$> (getDataFileName "day06-input.txt" >>= readFile)
+day06 = do
+  input <- getDataFileName "day06-input.txt" >>= readFile
+  return (solvePart1 input, solvePart2 input)
 
-solve :: String -> (Integer, Integer)
-solve input = (uncurry sumOfAllProblems $ numbersAndOperators, undefined)
+solvePart1 :: String -> Integer
+solvePart1 input = uncurry sumOfAllProblems numbersAndOperators
   where
-    numbersAndOperators = parseInput input
+    numbersAndOperators = parseInputPart1 input
 
     sumOfAllProblems :: [[Integer]] -> [Operator] -> Integer
     sumOfAllProblems numbersRows operatorsRow
@@ -25,8 +28,8 @@ solve input = (uncurry sumOfAllProblems $ numbersAndOperators, undefined)
 
 type Parser = P.Parsec Void String
 
-parseInput :: String -> ([[Integer]], [Operator])
-parseInput = either (error . P.errorBundlePretty) id . P.parse inputP ""
+parseInputPart1 :: String -> ([[Integer]], [Operator])
+parseInputPart1 = either (error . P.errorBundlePretty) id . P.parse inputP ""
   where
     inputP = do
       numbersRows <- P.some $ P.try (numbersRowP <* P.eol)
@@ -41,6 +44,29 @@ parseInput = either (error . P.errorBundlePretty) id . P.parse inputP ""
 
     operatorP :: Parser Operator
     operatorP = (Product <$ P.char '*') P.<|> (Sum <$ P.char '+')
+
+solvePart2 :: String -> Integer
+solvePart2 input = uncurry (sumOfAllProblems []) numberLinesAndOperators
+  where
+    numberLinesAndOperators = parseInputPart2 input
+
+    sumOfAllProblems :: [Integer] -> [[Char]] -> [Operator] -> Integer
+    sumOfAllProblems accNums numberLines operators
+      | all null numberLines = calc (head operators) accNums
+      | all (isSpace . head) numberLines =
+          calc (head operators) accNums + sumOfAllProblems [] (tail <$> numberLines) (tail operators)
+      | otherwise =
+          sumOfAllProblems (read (head <$> numberLines) : accNums) (tail <$> numberLines) operators
+
+parseInputPart2 :: String -> ([[Char]], [Operator])
+parseInputPart2 input = (init inputLines, parseOperators $ last inputLines)
+  where
+    inputLines = lines input
+    parseOperators = map parseOperator . filter (not . isSpace)
+    parseOperator = \case
+      '*' -> Product
+      '+' -> Sum
+      _unsupportedOperator -> error "unsupported operator"
 
 data Operator = Sum | Product deriving (Show, Eq)
 
@@ -61,14 +87,23 @@ tests = hspec $ do
             "*   +   *   +  "
           ]
 
-  describe "parseInput" $ do
+  describe "parseInputPart1" $ do
     specify "example input" $
-      parseInput exampleInputText
+      parseInputPart1 exampleInputText
         `shouldBe` ([[123, 328, 51, 64], [45, 64, 387, 23], [6, 98, 215, 314]], [Product, Sum, Product, Sum])
 
-  describe "solve" $ do
+  describe "parseInputPart2" $ do
     specify "example input" $
-      fst (solve exampleInputText) `shouldBe` 4277556
+      parseInputPart2 exampleInputText
+        `shouldBe` (["123 328  51 64 ", " 45 64  387 23 ", "  6 98  215 314"], [Product, Sum, Product, Sum])
+
+  describe "solvePart1" $ do
+    specify "example input" $
+      solvePart1 exampleInputText `shouldBe` 4277556
+
+  describe "solvePart2" $ do
+    specify "example input" $
+      solvePart2 exampleInputText `shouldBe` 3263827
 
   specify "day06" $ do
-    day06 >>= (`shouldBe` 5524274308182) . fst
+    day06 >>= (`shouldBe` (5524274308182, 8843673199391))
