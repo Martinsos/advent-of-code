f34bfb0aee8725520e4ae9a878105122b0258b38
Martin Sosic <sosic.martin@gmail.com>
2025-12-16 00:07:36 +0100

    Made day 06 nicer with generalizing solution via CCW rotation.

--- 2025/haskell/src/Day06.hs
+++ 2025/haskell/src/Day06.hs
@@ -7,50 +7,58 @@ import Paths_aoc (getDataFileName)
 import Test.Hspec (describe, hspec, shouldBe, specify)
 
 day06 :: IO (Integer, Integer)
-day06 = do
-  input <- getDataFileName "day06-input.txt" >>= readFile
-  return (solvePart1 input, solvePart2 input)
+day06 = solve <$> (getDataFileName "day06-input.txt" >>= readFile)
 
-solvePart1 :: String -> Integer
-solvePart1 input = uncurry sumOfAllProblems numbersAndOperators
+solve :: String -> (Integer, Integer)
+solve input = (calcMathWorksheet worksheet, calcMathWorksheet $ first rotateCCW <$> worksheet)
   where
-    numbersAndOperators = first (((read <$>) . words) <$>) $ parseInput input
+    worksheet = parseMathWorksheet input
 
-    sumOfAllProblems :: [[Integer]] -> [Operator] -> Integer
-    sumOfAllProblems numbersRows operatorsRow
-      | all null numbersRows && null operatorsRow = 0
-      | otherwise =
-          calc (head operatorsRow) (head <$> numbersRows)
-            + sumOfAllProblems (tail <$> numbersRows) (tail operatorsRow)
+type MathWorksheet = [MathProblem]
 
-solvePart2 :: String -> Integer
-solvePart2 input = uncurry (sumOfAllProblems []) numberLinesAndOperators
-  where
-    numberLinesAndOperators = parseInput input
+type MathProblem = (DigitsBlock, Operator)
 
-    sumOfAllProblems :: [Integer] -> [[Char]] -> [Operator] -> Integer
-    sumOfAllProblems accNums numberLines operators
-      | all null numberLines = calc (head operators) accNums
-      | all (isSpace . head) numberLines =
-          calc (head operators) accNums + sumOfAllProblems [] (tail <$> numberLines) (tail operators)
-      | otherwise =
-          sumOfAllProblems (read (head <$> numberLines) : accNums) (tail <$> numberLines) operators
-
-parseInput :: String -> ([[Char]], [Operator])
-parseInput input = (init inputLines, parseOperators $ last inputLines)
-  where
-    inputLines = lines input
-    parseOperators = map parseOperator . filter (not . isSpace)
-    parseOperator = \case
-      '*' -> Product
-      '+' -> Sum
-      _unsupportedOperator -> error "unsupported operator"
+type DigitsBlock = [[Char]]
 
 data Operator = Sum | Product deriving (Show, Eq)
+
+calcMathWorksheet :: MathWorksheet -> Integer
+calcMathWorksheet = sum . (calcMathProblem <$>)
+
+calcMathProblem :: MathProblem -> Integer
+calcMathProblem (digitsBlock, operator) = calc operator $ read <$> digitsBlock
 
 calc :: Operator -> [Integer] -> Integer
 calc Sum = sum
 calc Product = product
+
+parseMathWorksheet :: String -> MathWorksheet
+parseMathWorksheet input = go emptyAccLines $ lines input
+  where
+    go :: [String] -> [String] -> MathWorksheet
+    go accLines leftoverLines
+      | all null leftoverLines =
+          mathProblemFromAccLines : []
+      | all (isSpace . head) leftoverLines =
+          mathProblemFromAccLines : go emptyAccLines (tail <$> leftoverLines)
+      | otherwise =
+          go (zipWith (<>) accLines (pure . head <$> leftoverLines)) (tail <$> leftoverLines)
+      where
+        mathProblemFromAccLines = (init accLines, parseOperator $ last accLines)
+
+    emptyAccLines = repeat []
+
+    parseOperator :: String -> Operator
+    parseOperator =
+      ( \case
+          "*" -> Product
+          "+" -> Sum
+          op -> error $ "unsupported operator " <> op
+      )
+        . filter (not . isSpace)
+
+rotateCCW :: [[a]] -> [[a]]
+rotateCCW = foldr (zipWith (:) . reverse) (repeat [])
 
 ------------------------------------------------------------------------------------------
 
@@ -65,18 +73,46 @@ tests = hspec $ do
             "*   +   *   +  "
           ]
 
-  describe "parseInput" $ do
+  describe "parseMathWorksheet" $ do
     specify "example input" $
-      parseInput exampleInputText
-        `shouldBe` (["123 328  51 64 ", " 45 64  387 23 ", "  6 98  215 314"], [Product, Sum, Product, Sum])
+      parseMathWorksheet exampleInputText
+        `shouldBe` [ (["123", " 45", "  6"], Product),
+                     (["328", "64 ", "98 "], Sum),
+                     ([" 51", "387", "215"], Product),
+                     (["64 ", "23 ", "314"], Sum)
+                   ]
 
-  describe "solvePart1" $ do
-    specify "example input" $
-      solvePart1 exampleInputText `shouldBe` 4277556
+  specify "rotateCCW" $ do
+    let test (m1 :: [[Int]], m2) = rotateCCW m1 `shouldBe` m2
+    test
+      ( [ [1, 2, 3]
+        ],
+        [ [3],
+          [2],
+          [1]
+        ]
+      )
+    test
+      ( [ [1],
+          [2],
+          [3]
+        ],
+        [ [1, 2, 3]
+        ]
+      )
+    test
+      ( [ [1, 2, 3],
+          [4, 5, 6]
+        ],
+        [ [3, 6],
+          [2, 5],
+          [1, 4]
+        ]
+      )
 
-  describe "solvePart2" $ do
+  describe "solve" $ do
     specify "example input" $
-      solvePart2 exampleInputText `shouldBe` 3263827
+      solve exampleInputText `shouldBe` (4277556, 3263827)
 
   specify "day06" $ do
     day06 >>= (`shouldBe` (5524274308182, 8843673199391))
